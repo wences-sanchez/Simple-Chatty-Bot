097a691a8900f331018d55eb3d10ce8d7a54513f
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-01-13 13:16:32 +0100

    Second exercise completed

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,8 +1,27 @@
-print('Hello! My name is Christopher.')
+print('Hello! My name is Aid.')
 print('I was created in 2020.')
 print('Please, remind me your name.')
 
-# reading a name
-your_name = input()
+name = input()
 
-print(f'What a great name you have, {your_name}!')
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+rem3 = int(input())
+rem5 = int(input())
+rem7 = int(input())
+
+age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+
+# read a number and count to it here
+number = int(input())
+cont = 0
+while cont <= number:
+    print(f'{cont} !')
+    cont += 1
+
+print('Completed, have a nice day!')
